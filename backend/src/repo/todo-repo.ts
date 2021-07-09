d265fc43a4cbc610b5f1a23f335f07e6bb7c28c7
oblivion8514 <oblivion8514@gmail.com>
2021-07-09 09:01:32 +0000

    implementation hhtangb

--- backend/src/repo/todo-repo.ts
+++ backend/src/repo/todo-repo.ts
@@ -2,10 +2,10 @@ import { ITodo } from './../types/todo'
 import Todo from './../models/todo'
 
 interface TodoRepo {
-    getTodos(): Promise<Array<ITodo>>
+    //getTodos(): Promise<Array<ITodo>>
     addTodo(todoBody: ITodo): Promise<ITodo>
-    updateTodo(id: string, todoBody: ITodo): Promise<ITodo | null>
-    deleteTodo(id: string): Promise<ITodo | null>
+    //updateTodo(id: string, todoBody: ITodo): Promise<ITodo | null>
+    //deleteTodo(id: string): Promise<ITodo | null>
 }
 
 class TodoRepoImpl implements TodoRepo {
@@ -16,18 +16,19 @@ class TodoRepoImpl implements TodoRepo {
     }
 
     async getTodos(): Promise<Array<ITodo>> {
-        // TODO: Should get Todo from mongoDB
+        return Todo.find()
     }
 
-    // TODO: Should add Todo into mongoDB
-
+    async addTodo(todoBody: ITodo): Promise<ITodo> {
+        return Todo.create(todoBody)
+    }
 
     async updateTodo(id: string, todoBody: ITodo): Promise<ITodo | null> {
-        // TODO: Should update Todo to mongoDB
+        return Todo.findByIdAndUpdate(id, todoBody)
     }
 
     async deleteTodo(id: string): Promise<ITodo | null> {
-        // TODO: Should delete Todo from mongoDB
+        return Todo.findByIdAndDelete(id)
     }
 
 }
--- backend/src/routes/todo.ts
+++ backend/src/routes/todo.ts
@@ -12,6 +12,44 @@ const TodoRouter = (server: FastifyInstance, opts: RouteShorthandOptions, done: 
 
     // TODO: Add CRUD endpoints, i.e. get, post, update, delete
     // NOTE: the url should be RESTful
+    server.get ('/todos', opts, async (request, reply) => {
+        const todos: Array<ITodo> = await todoRepo.getTodos()
+        return reply.status(200).send({ todos })
+    })
+    server.post('/todos',opts,async (request, reply) =>{
+        const todoBody: ITodo = request.body as ITodo;
+        const todo: ITodo = await todoRepo.addTodo(todoBody)
+        return reply.status(201).send({todo})
+    })
+
+    server.put<{ Params: IdParam }>('/todos/:id', opts, async (request, reply) => {
+        try {
+            const id = request.params.id
+            const todoBody = request.body as ITodo
+            const todo: ITodo | null = await todoRepo.updateTodo(id, todoBody)
+            if (todo) {
+                return reply.status(200).send({ todo })
+            } else {
+                return reply.status(404).send({ msg: `Not Found Todo:${id}` })
+            }
+        } catch (error) {
+            console.error(`PUT /todos/${request.params.id} Error: ${error}`)
+            return reply.status(500).send(`[Server Error]: ${error}`)
+        }
+    })
+
+    server.delete<{ Params: IdParam }>('/todos/:id', opts, async (request, reply) => {
+        const id = request.params.id
+        const todo: ITodo | null = await todoRepo.deleteTodo(id)
+        if (todo) {
+            return reply.status(204).send()
+        } else {
+            return reply.status(404).send({ msg: `Not Found Todo:${id}` })
+        }
+    })
+
+
+    //server.get('/')
 
     done()
 }
